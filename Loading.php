ae3f02ac96930d04455ccf592dd480a9e2917889
izzdanishadam <146014905+izzdanishadam@users.noreply.github.com>
2023-10-09 01:41:50 +0800

    Add files via upload

---
+++ Loading.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$i = 0;
+while(true){
+    if (!empty($_SESSION['id'][$i])){
+        $i = $i + 1;
+    } else {
+        break;
+    }
+} 
+
+$_SESSION["username"] = $_POST["username"];
+$email = $_SESSION["username"];
+
+$con = mysqli_connect("localhost","root","","izzcondo");
+$query = "SELECT `userRole` FROM `users` WHERE `userName` = '$email'"; 
+$result = $con->query($query);
+
+// Check if the query executed successfully
+if ($result) {
+    // Fetch data from the result object
+    while ($row = $result->fetch_assoc()) {
+        if ($row["userRole"] == "admin"){
+            header("Location: AdminDashboard.html");
+        } elseif($row["userRole"] == "user"){
+            header("Location: TenantHome.html");
+        }
+    }
+
+    // Free the result set
+    $result->free();
+} else {
+    // Handle the error if the query fails
+    echo "Error: " . $con->error;
+}
+
+// Close the connection
+$con->close();
+?>
