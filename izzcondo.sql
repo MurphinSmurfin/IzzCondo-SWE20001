381a952c7199180b86abdf9864a39b13134f7091
izzdanishadam <146014905+izzdanishadam@users.noreply.github.com>
2023-10-10 14:27:34 +0800

    Update izzcondo.sql
    added new table "units", edited users table and added more collumns

--- izzcondo.sql
+++ izzcondo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2023 at 08:55 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Oct 10, 2023 at 08:25 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `unitId` int(4) NOT NULL,
+  `unitBlock` char(1) NOT NULL,
+  `unitNumber` int(2) NOT NULL,
+  `unitFloor` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`unitId`, `unitBlock`, `unitNumber`, `unitFloor`) VALUES
+(1, 'B', 10, 25),
+(2, 'A', 4, 18),
+(3, 'B', 6, 14),
+(4, 'B', 13, 27),
+(5, 'A', 15, 12);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,20 +55,34 @@ CREATE TABLE `users` (
   `userId` int(6) NOT NULL,
   `userName` varchar(30) NOT NULL,
   `userPass` varchar(30) NOT NULL,
-  `userRole` varchar(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `userEmail` varchar(255) NOT NULL,
+  `userRole` varchar(6) NOT NULL,
+  `unitId` int(4) NOT NULL,
+  `paymentStatus` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userId`, `userName`, `userPass`, `userRole`) VALUES
-(1, 'admin', 'admin', 'admin'),
-(2, 'user', 'user', 'user');
+INSERT INTO `users` (`userId`, `userName`, `userPass`, `userEmail`, `userRole`, `unitId`, `paymentStatus`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', 'admin', 0, 'paid'),
+(2, 'user', 'user', 'user@gmail.com', 'user', 1, 'paid'),
+(3, 'Izz Danish', '123', 'izzdanishadam@gmail.com', 'user', 4, 'pending'),
+(4, 'Hiew Wei Lik', '1234', 'cnbtorture69@gmail.com', 'admin', 2, 'overdue'),
+(5, 'CF', 'cfnotepad', 'notepaduser@gmail.com', 'user', 3, 'overdue'),
+(6, 'Vickram', '12345', 'dndnd@gmail.com', 'user', 5, 'unpaid'),
+(7, 'CY', 'designer', 'designer@wix.com', 'user', 1, 'pending');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`unitId`);
 
 --
 -- Indexes for table `users`
@@ -60,7 +98,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `userId` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
