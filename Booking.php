9044f77d80ec2626cc6c0154be34a960b4fb15d8
izzdanishadam <146014905+izzdanishadam@users.noreply.github.com>
2023-11-23 03:08:53 +0800

    Update Booking.php

--- Booking.php
+++ Booking.php
@@ -158,7 +158,7 @@ background-color: rgba(0,0,0,0.4); /* Black w/ opacity */">
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="Image\gym.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
                 <div class="card-body">
                   <h4 class="card-title gym">Gym Room</h4>
-                  <p class="card-text">Elevate your fitness journey in our cutting-edge gym room. Equipped with state-of-the-art facilities, it's your space for strength, cardio, and wellness. Get ready to sweat and achieve your fitness goals. Book your workout time now!</p>
+                  <p class="card-text">Elevate your fitness journey in our cutting-edge gym room. Equipped with state-of-the-art facilities, it's your space for strength, cardio, and wellness. Get ...</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <input type="submit" value="Booking" class="btn btn-sm btn-outline-secondary" name="btnGym"></input>
@@ -187,26 +187,11 @@ background-color: rgba(0,0,0,0.4); /* Black w/ opacity */">
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="Image\yoga.jpg" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
                 <div class="card-body">
                   <h4 class="card-title yoga">Yoga Studio</h4>
-                  <p class="card-text">Find serenity in our tranquil yoga room. Unwind, stretch, and meditate in a peaceful ambiance. Embrace mindfulness and inner peace. Reserve your spot for a rejuvenating yoga session now!</p>
+                  <p class="card-text">Find serenity in our tranquil yoga room. Unwind, stretch, and meditate in a peaceful ambiance. Embrace mindfulness and inner peace. Reserve your spot for a ...</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <input type="submit" value="Booking" class="btn btn-sm btn-outline-secondary" name="btnYoga"></input>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18b14afb781%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18b14afb781%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.71875%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
                   </div>
                 </div>
               </div>
