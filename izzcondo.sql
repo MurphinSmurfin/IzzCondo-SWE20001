550f7a909e61d2a45fbe9fb72c683872535b6c99
izzdanishadam <146014905+izzdanishadam@users.noreply.github.com>
2023-11-23 15:44:17 +0800

    Update izzcondo.sql

--- izzcondo.sql
+++ izzcondo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2023 at 08:52 PM
+-- Generation Time: Nov 23, 2023 at 08:43 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
@@ -31,16 +31,19 @@ CREATE TABLE `announcement` (
   `header` text NOT NULL,
   `title` text NOT NULL,
   `date` text NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `announcementId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `announcement`
 --
 
-INSERT INTO `announcement` (`header`, `title`, `date`, `description`) VALUES
-('test', 'test', '22/11/2023', 'testtestsetsetsetset'),
-('2yersdy', '2yrdy', '22/11/2023', '3ysdtsdtst');
+INSERT INTO `announcement` (`header`, `title`, `date`, `description`, `announcementId`) VALUES
+('Gym Renovation Announcement', 'Gym Renovation 13/11 - 21/11', '23/11/2023', 'We are sorry to inform you that the gym will be shutdown temporarily due to renovation until further notice.', 3),
+('Water Disruption Announcement', 'Water Shortage', '23/11/2023', 'A pipe burst was located and is currently being fixed. Sorry for the inconvenience', 4),
+('Booking System Down', 'Booking System Under Maintenance', '23/11/2023', 'Please proceed to Main Office to book facilities.', 5),
+('Maintenance on Pool', 'Pool Maintenance on December', '23/11/2023', 'Pool will be closed for maintenance on December 2023.', 6);
 
 -- --------------------------------------------------------
 
@@ -61,7 +64,10 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`bookingId`, `bookingVenue`, `bookingDate`, `bookingTime`, `userId`) VALUES
-(1, 'Badminton', '2023-10-06', '14:54:00', 2);
+(1, 'Badminton', '2023-10-06', '14:54:00', 2),
+(2, 'Basketball', '2023-11-23', '05:07:00', 3),
+(3, 'Basketball', '2023-11-23', '17:06:00', 3),
+(4, 'Basketball', '2023-11-23', '19:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -82,7 +88,9 @@ CREATE TABLE `inbox` (
 
 INSERT INTO `inbox` (`inboxId`, `userId`, `subject`, `content`) VALUES
 (1, 2, 'Plumbing Issue', 'Okay! Sending plumbers on your way!'),
-(2, 2, 'Pest Issue', 'ew pests, sending hitler\'s friends');
+(3, 3, 'Plumbing Maintenance Request', 'Plumbers are on the way! They brought Mario.'),
+(4, 3, 'Parking Request Approved', 'Your parking request for Block B, Floor 3 has been approved. Your parking slot will be in B-3-8.'),
+(5, 3, 'Stuck Cat', 'Mario on the way to take your cat out.');
 
 -- --------------------------------------------------------
 
@@ -112,7 +120,11 @@ INSERT INTO `parking` (`parkingId`, `userId`, `block`, `floor`, `unit`) VALUES
 (7, 7, 'B', 3, 2),
 (8, 2, 'B', 1, 5),
 (9, 2, 'B', 3, 3),
-(10, 2, 'B', 3, 6);
+(10, 2, 'B', 3, 6),
+(11, 3, 'B', 3, 8),
+(13, 3, 'B', 1, 11),
+(14, 2, 'A', 2, 12),
+(15, 3, 'A', 2, 13);
 
 -- --------------------------------------------------------
 
@@ -136,8 +148,10 @@ CREATE TABLE `requests` (
 INSERT INTO `requests` (`requestId`, `userId`, `requestType`, `problem`, `description`, `unitId`) VALUES
 (1, 2, 'Maintenance Request', 'Plumbing Issue', 'test', 1),
 (2, 3, 'Maintenance Request', 'Plumbing Issue', '2323', 4),
-(3, 3, 'Parking Request', 'n/a', 'B-2-8', 4),
-(4, 2, 'Maintenance Request', 'Pest Issue', 'blegh pests', 1);
+(4, 2, 'Maintenance Request', 'Pest Issue', 'blegh pests', 1),
+(5, 3, 'Parking Request', 'n/a', 'Block B, Floor 3', 4),
+(6, 3, 'Maintenance Request', 'Plumbing Issue', 'Help my cat is stuck in the sink', 4),
+(7, 3, 'Parking Request', 'n/a', 'Block A, Floor 2', 4);
 
 -- --------------------------------------------------------
 
@@ -190,11 +204,18 @@ INSERT INTO `users` (`userId`, `userName`, `userPass`, `userEmail`, `userRole`, 
 (4, 'Hiew Wei Lik', '1234', 'cnbtorture69@gmail.com', 'admin', 2, 'overdue'),
 (5, 'CF', 'cfnotepad', 'notepaduser@gmail.com', 'user', 3, 'overdue'),
 (6, 'Vickram', '12345', 'dndnd@gmail.com', 'user', 5, 'unpaid'),
-(7, 'CY', 'designer', 'designer@wix.com', 'user', 1, 'pending');
+(7, 'CY', 'designer', 'designer@wix.com', 'user', 1, 'pending'),
+(9, 'Faizal', 'doesntmatter', 'faizal@gmail.com', 'user', 3, 'paid');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `announcement`
+--
+ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`announcementId`);
 
 --
 -- Indexes for table `booking`
@@ -239,34 +260,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `announcement`
+--
+ALTER TABLE `announcement`
+  MODIFY `announcementId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `inbox`
 --
 ALTER TABLE `inbox`
-  MODIFY `inboxId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `inboxId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `parking`
 --
 ALTER TABLE `parking`
-  MODIFY `parkingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `parkingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `requestId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `requestId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `userId` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
